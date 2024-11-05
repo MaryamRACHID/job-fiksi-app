3f478106c04a5f939cd9034dc9cb83a7e8f6e7ce
Ferdaousse24 <elbaraka23ferdaousse@gmail.com>
2024-11-06 00:22:48 +0100

    Accueil restaurant version 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
+// src/app/app.component.ts
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterModule, AccueilComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  showAuthContainer: boolean = true;
-
-  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
-
-  ngOnInit() {
-    // Surveille la route actuelle pour cacher ou afficher l'auth-container
-    this.router.events.subscribe(() => {
-      this.showAuthContainer = this.router.url !== '/home';
-    });
-  }
-}
+export class AppComponent { }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
+// src/app/app.routes.ts
 import { Routes } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: AccueilComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' }, // Redirection par défaut
+  { path: '**', redirectTo: 'home' } // Redirection pour les routes non trouvées
+];
